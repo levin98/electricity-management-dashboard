18d1286eb07e0adb4f5b63d77ca19f65cf1f0b5a
Kelvin <kelvinlim_98@live.com>
2020-04-22 11:49:42 +0800

    Added settings vue form data

--- src/pages/Settings.vue
+++ src/pages/Settings.vue
@@ -4,22 +4,22 @@
       <h6 class="title d-inline">Electricity Cost Configurations</h6>
     </template>
     <form @submit.prevent>
-      <base-input label="First 200 kWh (1 - 200 kWh) cost" placeholder="RM 0.218">
+      <base-input label="First 200 kWh (1 - 200 kWh) cost" v-model="form.first200" placeholder="RM 0.218">
           <small slot="helperText" id="emailHelp" class="form-text text-muted">For the first 200 kWh (1 - 200 kWh) per month</small>
       </base-input>
-      <base-input label="Next 100 kWh (201 - 300 kWh) cost" placeholder="RM 0.334">
+      <base-input label="Next 100 kWh (201 - 300 kWh) cost" v-model="form.next100" placeholder="RM 0.334">
           <small slot="helperText" id="emailHelp" class="form-text text-muted">For the next 100 kWh (201 - 300 kWh) per month</small>
       </base-input>
-      <base-input label="Next 300 kWh (301 - 600 kWh) cost" placeholder="RM 0.516">
+      <base-input label="Next 300 kWh (301 - 600 kWh) cost" v-model="form.next300" placeholder="RM 0.516">
           <small slot="helperText" id="emailHelp" class="form-text text-muted">For the next 300 kWh (301 - 600 kWh) per month</small>
       </base-input>
-      <base-input label="Next 300 kWh (601 - 900 kWh) cost" placeholder="RM 0.546">
+      <base-input label="Next 300 kWh (601 - 900 kWh) cost" v-model="form.next600" placeholder="RM 0.546">
           <small slot="helperText" id="emailHelp" class="form-text text-muted">For the next 300 kWh (601 - 900 kWh) per month</small>
       </base-input>
-      <base-input label="Next kWh (901 kWh onwards) cost" placeholder="RM 0.571">
+      <base-input label="Next kWh (901 kWh onwards) cost" v-model="form.next900" placeholder="RM 0.571">
           <small slot="helperText" id="emailHelp" class="form-text text-muted">For the next kWh (901 kWh onwards) per month</small>
       </base-input>
-      <base-input label="Minimum monthly cost" placeholder="RM 3.00">
+      <base-input label="Minimum monthly cost" v-model="form.mincost" placeholder="RM 3.00">
           <small slot="helperText" id="emailHelp" class="form-text text-muted">The minimum monthly charge</small>
       </base-input>
      <base-button native-type="submit" type="primary">Save</base-button>
@@ -37,6 +37,14 @@ export default {
   },
   data() {
     return {
+      form: {
+        first200: '',
+        next100: '',
+        next300: '',
+        next600: '',
+        next900: '',
+        mincost: ''
+      }
     }
   }
 };
